6473be70dfc2e41cb9ac3185032ec6b0dc5bf4ca
55Nights <1905587@students.kcau.ac.ke>
2023-04-29 17:43:54 +0300

    second commit

---
+++ alembic/versions/e31ba0e153e4_hello.py
@@ -0,0 +1,24 @@
+"""hello
+
+Revision ID: e31ba0e153e4
+Revises: 96671e2b62a6
+Create Date: 2023-04-28 23:09:43.268350
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e31ba0e153e4'
+down_revision = '96671e2b62a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
